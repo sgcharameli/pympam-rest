4bfb5fcffbca97e30f79c1c9c236a198eb3d708c
Sergio García Charameli <sgcharameli@gmail.com>
2020-02-06 22:59:26 +0100

    Commit inicial

---
+++ src/main/java/es/sv/pympam/pympamrest/PympamRestApplication.java
@@ -0,0 +1,15 @@
+package es.sv.pympam.pympamrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
+public class PympamRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PympamRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/sv/pympam/pympamrest/PympamRestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.sv.pympam.pympamrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PympamRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
